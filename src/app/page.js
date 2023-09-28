8f40dd60aa819042edbdd937a948cb17bcec158b
kamilo9809 <camilorivera09@outlook.com>
2023-09-28 09:54:08 -0500

    inicion del proceso

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,6 @@
 export default function Home() {
-
-  function Cancion(){
-    return<div>
-      <h1>hola mundon</h1>
-      <Cancion titulo="la cucaracha" descripcion="ya no puede caminar" />
-    </div>;
-  }
-
-
-
-
   return (
-    <main className="container mx auto p-4 h-screen flex item-center justify-center">
-        <h1>esto va a ser un ejemplo de props</h1>
-        <canciones titulo="la cucaracha" descripcion="la cucaracha la cucaracha ya no puede caminar" />
+    <main className="">
     </main>
   )
 }

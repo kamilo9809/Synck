99884472b9f07edf0429cd09d75e0443cbe3a353
kamilo9809 <camilorivera09@outlook.com>
2023-09-28 17:06:44 -0500

    actualizacion pages
    avanze estilos y navbar

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "componentes/app/components/navbar";
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <Navbar />
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundColor:{
+        'color-gris':'#434343'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':

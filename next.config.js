d8fe9875383054cc7d27b2780021d574b65e8f02
kamilo9809 <camilorivera09@outlook.com>
2023-09-29 15:43:07 -0500

    actualizacion de seguridad

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const { i18n } = require("next-i18next");
+const withFonts = require('next-fonts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -10,5 +11,16 @@ const nextConfig = {
     }
 }
 
-
+module.exports = withFonts({
+    webpack(config, options) {
+      return config;
+    },
+    fonts: [
+      {
+        family: 'Archivo Black',
+        fileFormats: ['woff', 'woff2'],
+        fontDisplay: 'swap',
+      },
+    ],
+  });
 module.exports = nextConfig
--- src/app/body.jsx
+++ src/app/body.jsx
@@ -1,21 +1,27 @@
-
 import Image from "next/image";
 
 
-export default function Body(){
-
-    return(
-        <div>
-          <div className="bg-color-gris flex flex-row">
-          <Image className="fixed" src="/images/PLAY BLUR.svg" alt="" width={400} height={400} />  
-            <div className="flex flex-col items-center justify-center">
-              <h2 className="text-white ">CREA EXPONE CONECTA</h2>                   
-              <p className="text-white">Sube y comparte tu música donde los artistas y productores conectan a través de sus habilidades. </p>            
-            </div>
-            <div>
-             <Image src="/images/INTERFAZ PERS 1.svg" alt="person image" width={585} height={734} />
-            </div>
-          </div>
+export default function Body() {
+  return (
+    <div>
+      <Image className="fixed" src="/images/PLAY BLUR.svg" alt="" width={400} height={400} />
+      <div className="bg-color-gris flex flex-row items-center justify-center">
+        <div className="flex flex-col items-center justify-center pb-14" width={585} height={734}>
+          <div className="w-345 h-210 flex justify-center items-center">
+            <h1 className="tamaño-texto text-white font-bold">
+              CREA{' '} <br />
+              <span className="customtext fixed font-bold blur-sm">EXPONE</span>
+              <span className="customtext">EXPONE</span> <br /> CONECTA
+            </h1>
         </div>
-    );
-} 
+        <div className="parrafo1">
+          <p className="parrafo1 text-white">Sube y comparte tu música donde los artistas y productores conectan a través de sus habilidades.</p>
+        </div>
+        </div>
+        <div>
+          <Image src="/images/INTERFAZ PERS 1.svg" alt="person image" width={585} height={734} />
+        </div>
+      </div>
+    </div>
+  );
+}
